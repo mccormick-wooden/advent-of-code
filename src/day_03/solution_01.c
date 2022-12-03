8faf1e395f470b1dda3e9d4a6e31956f93b276ba
McCormick Wooden <mwooden3@gatech.edu>
2022-12-03 17:32:55 -0600

    day 3 solution 1

--- src/day_03/solution_01.c
+++ src/day_03/solution_01.c
@@ -1,82 +1,85 @@
-// --- Day 2: Rock Paper Scissors ---
+// --- Day 3: Rucksack Reorganization ---
 
-// The Elves begin to set up camp on the beach. To decide whose tent gets to be closest to the snack storage,
-// a giant Rock Paper Scissors tournament is already in progress.
+// One Elf has the important job of loading all of the rucksacks with supplies for the jungle journey. Unfortunately, that
+// Elf didn't quite follow the packing instructions, and so a few items now need to be rearranged.
 
-// Rock Paper Scissors is a game between two players. Each game contains many rounds; in each round, the players
-// each simultaneously choose one of Rock, Paper, or Scissors using a hand shape. Then, a winner for that round is
-// selected: Rock defeats Scissors, Scissors defeats Paper, and Paper defeats Rock. If both players choose the same
-// shape, the round instead ends in a draw.
+// Each rucksack has two large compartments. All items of a given type are meant to go into exactly one of the two
+// compartments. The Elf that did the packing failed to follow this rule for exactly one item type per rucksack.
 
-// Appreciative of your help yesterday, one Elf gives you an encrypted strategy guide (your puzzle input) that they
-// say will be sure to help you win. "The first column is what your opponent is going to play: A for Rock, B for Paper,
-// and C for Scissors. The second column--" Suddenly, the Elf is called away to help with someone's tent.
+// The Elves have made a list of all of the items currently in each rucksack (your puzzle input), but they need your help
+//  finding the errors. Every item type is identified by a single lowercase or uppercase letter (that is, a and A refer to
+//   different types of items).
 
-// The second column, you reason, must be what you should play in response: X for Rock, Y for Paper, and Z for Scissors.
-// Winning every time would be suspicious, so the responses must have been carefully chosen.
+// The list of items for each rucksack is given as characters all on a single line. A given rucksack always has the same
+//  number of items in each of its two compartments, so the first half of the characters represent items in the first
+//   compartment, while the second half of the characters represent items in the second compartment.
 
-// The winner of the whole tournament is the player with the highest score. Your total score is the sum of your scores
-// for each round. The score for a single round is the score for the shape you selected (1 for Rock, 2 for Paper, and 3
-// for Scissors) plus the score for the outcome of the round (0 if you lost, 3 if the round was a draw, and 6 if you won).
+// For example, suppose you have the following list of contents from six rucksacks:
 
-// Since you can't be sure if the Elf is trying to help you or trick you, you should calculate the score you would get
-// if you were to follow the strategy guide.
+// vJrwpWtwJgWrhcsFMMfFFhFp
+// jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+// PmmdzqPrVvPwwTWBwg
+// wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+// ttgJtRGJQctTZtZT
+// CrZsJsPPZsGzwwsLwLmpwMDw
 
-// For example, suppose you were given the following strategy guide:
+//     - The first rucksack contains the items vJrwpWtwJgWrhcsFMMfFFhFp, which means its first compartment contains
+//         the items vJrwpWtwJgWr, while the second compartment contains the items hcsFMMfFFhFp. The only item type that
+//         appears in both compartments is lowercase p.
+//     - The second rucksack's compartments contain jqHRNqRjqzjGDLGL and rsFMfFZSrLrFZsSL. The only item type that
+//         appears in both compartments is uppercase L.
+//     - The third rucksack's compartments contain PmmdzqPrV and vPwwTWBwg; the only common item type is uppercase P.
+//     - The fourth rucksack's compartments only share item type v.
+//     - The fifth rucksack's compartments only share item type t.
+//     - The sixth rucksack's compartments only share item type s.
 
-// A Y
-// B X
-// C Z
+// To help prioritize item rearrangement, every item type can be converted to a priority:
 
-// This strategy guide predicts and recommends the following:
+//     Lowercase item types a through z have priorities 1 through 26.
+//     Uppercase item types A through Z have priorities 27 through 52.
 
-//     - In the first round, your opponent will choose Rock (A), and you should choose Paper (Y). This ends in a
-//       win for you with a score of 8 (2 because you chose Paper + 6 because you won).
-//     - In the second round, your opponent will choose Paper (B), and you should choose Rock (X). This ends in a
-//       loss for you with a score of 1 (1 + 0).
-//     - The third round is a draw with both players choosing Scissors, giving you a score of 3 + 3 = 6.
+// In the above example, the priority of the item type that appears in both compartments of each rucksack is
+// 16 (p), 38 (L), 42 (P), 22 (v), 20 (t), and 19 (s); the sum of these is 157.
 
-// In this example, if you were to follow the strategy guide, you would get a total score of 15 (8 + 1 + 6).
-
-// What would your total score be if everything goes exactly according to your strategy guide?
-
-// A, X = ROCK. Score = 1
-// B, Y = PAPER. Score = 2
-// C, Z = Scissors. Score = 3
-
-// score = shapeYouChose + outcome
-// win = 6, draw = 3, lose = 0
+// Find the item type that appears in both compartments of each rucksack. What is the sum of the priorities
+// of those item types?
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+#include <assert.h>
 
 const char *INPUT_FILE_PATH = "input.txt";
+const int ASCII_LOWER_OFFSET = -96;
+const int ASCII_UPPER_OFFSET = 58 + ASCII_LOWER_OFFSET;
 
-typedef struct moves {
-    int len;
-    char *oppMoves;
-    char *yourMoves;
-} moves_t;
-
-void countLine_itercbk(char *line, int lineNumber, void *returnptr) {
-    (*(int*)returnptr)++;
+int calcItemPriority(char item) {
+    int score = isupper(item) ? item + ASCII_UPPER_OFFSET : item + ASCII_LOWER_OFFSET;
+    assert(score >= 1 && score <= 52);
+    return score;
 }
 
-void populateMoves_itercbk(char *line, int lineNumber, void *returnptr) {
-    moves_t *moves = (moves_t*)returnptr;
+void calcPriority_itercbk(char *line, ssize_t lineLength, int lineNumber, void *returnptr) {
+    int firstHalfStart = 0;
+    int firstHalfEnd = (lineLength - 1) / 2;
+    int secondHalfStart = firstHalfEnd;
+    int secondHalfEnd = lineLength - 1;
+    char itemMatch = '\0';
 
-    moves->oppMoves[lineNumber] = line[0];
+    for (int i = firstHalfStart; i < firstHalfEnd; i++) {
+        if (itemMatch != '\0')
+            break;
+        for (int j = secondHalfStart; j < secondHalfEnd; j++) {
+            if (line[i] == line[j]) {
+                itemMatch = line[i];
+                break;
+            }
+        }
+    }
 
-    // normalize
-    char normalized = '\0';
-    if (line[2] == 'X')
-        normalized = 'A';
-    else if (line[2] == 'Y')
-        normalized = 'B';
-    else if (line[2] == 'Z')
-        normalized = 'C';
-    moves->yourMoves[lineNumber] = normalized;
+    assert(isalpha(itemMatch));
+    (*(int*)returnptr) = (*(int*)returnptr) + calcItemPriority(itemMatch);
 }
 
 void fileIterator(FILE *fp, void (*callback)(), void *cbkretptr) {
@@ -87,64 +90,13 @@ void fileIterator(FILE *fp, void (*callback)(), void *cbkretptr) {
 
     rewind(fp);
     while((read = getline(&line, &len, fp)) != -1) {
-        (*callback)(line, lineNumber, cbkretptr);
-        lineNumber++;
+        if (callback != NULL) {
+            (*callback)(line, read, lineNumber, cbkretptr);
+            lineNumber++;
+        }
     }
     free(line); // gets malloc'd in getline so we have to free
     rewind(fp);
-}
-
-int calculateScore(moves_t *theMoves) {
-    int totalScore = 0;
-    const int YOUR_MOVE_SCORE_ASCII_OFFSET = 64;
-    const int WIN_SCORE = 6;
-    const int DRAW_SCORE = 3;
-    const int LOSS_SCORE = 0;
-
-    for (int i = 0; i < theMoves->len; i++) {
-
-        char oppMove = theMoves->oppMoves[i];
-        char yourMove = theMoves->yourMoves[i];
-        int yourMoveScore = yourMove - YOUR_MOVE_SCORE_ASCII_OFFSET; // 'A', 'B', 'C' ascii codes = 65, 66, 67
-
-        int winLossScore = DRAW_SCORE;
-        switch(oppMove) {
-            case 'A':
-                switch(yourMove) {
-                    case 'B':
-                        winLossScore = WIN_SCORE;
-                        break;
-                    case 'C':
-                        winLossScore = LOSS_SCORE;
-                        break;
-                }
-                break;
-            case 'B':
-                switch(yourMove) {
-                    case 'A':
-                        winLossScore = LOSS_SCORE;
-                        break;
-                    case 'C':
-                        winLossScore = WIN_SCORE;
-                        break;
-                }
-                break;
-            case 'C':
-                switch(yourMove) {
-                    case 'A':
-                        winLossScore = WIN_SCORE;
-                        break;
-                    case 'B':
-                        winLossScore = LOSS_SCORE;
-                        break;
-                }
-                break;
-        }
-
-        totalScore = totalScore + winLossScore + yourMoveScore;
-    }
-
-    return totalScore;
 }
 
 int main() {
@@ -152,23 +104,10 @@ int main() {
     if ((fp = fopen(INPUT_FILE_PATH, "r")) == NULL)
         return 1;
 
-    // get file length
-    int lineCount = 0;
-    fileIterator(fp, &countLine_itercbk, &lineCount);
+    int prioritySum = 0;
+    fileIterator(fp, &calcPriority_itercbk, &prioritySum);
 
-    // init main data structure to hold moves
-    printf("%d\n", lineCount);
-    char oppMoves[lineCount];
-    char yourMoves[lineCount];
-    memset(oppMoves, 0, sizeof lineCount);
-    memset(yourMoves, 0, sizeof lineCount);
-    moves_t theMoves = {.len = lineCount, .oppMoves = oppMoves, .yourMoves = yourMoves};
-
-    // populate moves data structure
-    fileIterator(fp, &populateMoves_itercbk, &theMoves);
-
-    // done
-    printf("total score: %d\n", calculateScore(&theMoves));
+    printf("priority sum: %d\n", prioritySum);
 
     // cleanup
     fclose(fp);
