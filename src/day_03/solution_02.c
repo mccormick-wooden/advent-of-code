aa04b4e7591c80a0cb481125dd56573ea53311c4
McCormick Wooden <mwooden3@gatech.edu>
2022-12-03 18:13:32 -0600

    day 3 solution 2, gross

--- src/day_03/solution_02.c
+++ src/day_03/solution_02.c
@@ -50,20 +50,25 @@ int calcItemPriority(char item) {
     return score;
 }
 
-void calcPriority_itercbk(char *line, ssize_t lineLength, int lineNumber, void *returnptr) {
-    int firstHalfStart = 0;
-    int firstHalfEnd = (lineLength - 1) / 2;
-    int secondHalfStart = firstHalfEnd;
-    int secondHalfEnd = lineLength - 1;
+void calcPriority_itercbk(char *line1, ssize_t lineLength1,
+                            char *line2, ssize_t lineLength2,
+                            char *line3, ssize_t lineLength3, int lineNumber, void *returnptr) {
+
     char itemMatch = '\0';
 
-    for (int i = firstHalfStart; i < firstHalfEnd; i++) {
+    for (int i = 0; i<lineLength1-1; i++) {
         if (itemMatch != '\0')
             break;
-        for (int j = secondHalfStart; j < secondHalfEnd; j++) {
-            if (line[i] == line[j]) {
-                itemMatch = line[i];
+        for (int j = 0; j<lineLength2-1; j++) {
+            if (itemMatch != '\0')
                 break;
+            else if (line1[i] != line2[j])
+                continue;
+            for (int k = 0; k<lineLength3-1; k++) {
+                if (line3[k] == line2[j]) {
+                    itemMatch = line3[k];
+                    break;
+                }
             }
         }
     }
@@ -73,18 +78,44 @@ void calcPriority_itercbk(char *line, ssize_t lineLength, int lineNumber, void *
 }
 
 void fileIterator(FILE *fp, void (*callback)(), void *cbkretptr) {
+    rewind(fp);
     char* line = NULL;
     size_t len = 0;
     ssize_t read;
     int lineNumber = 0;
 
-    rewind(fp);
+    char line1[128];
+    char line2[128];
+    char line3[128];
+    memset(line1, 0, sizeof line1);
+    memset(line2, 0, sizeof line2);
+    memset(line3, 0, sizeof line3);
+    ssize_t lineLength1 = 0;
+    ssize_t lineLength2 = 0;
+    ssize_t lineLength3 = 0;
+
     while((read = getline(&line, &len, fp)) != -1) {
-        if (callback != NULL) {
-            (*callback)(line, read, lineNumber, cbkretptr);
+        if (line1[0] == 0) {
+            strncpy(line1, (const char*)line, read);
+            lineLength1 = read;
+        } else if (line2[0] == 0) {
+            strncpy(line2, (const char*)line, read);
+            lineLength2 = read;
+        } else if (line3[0] == 0) {
+            strncpy(line3, (const char*)line, read);
+            lineLength3 = read;
+        }
+
+        if (callback != NULL && line1[0] != 0 && line2[0] != 0 && line3[0] != 0) {
+            (*callback)(line1, lineLength1, line2, lineLength2, line3, lineLength3, lineNumber, cbkretptr);
             lineNumber++;
+
+            memset(line1, 0, sizeof line1);
+            memset(line2, 0, sizeof line2);
+            memset(line3, 0, sizeof line3);
         }
     }
+    
     free(line); // gets malloc'd in getline so we have to free
     rewind(fp);
 }
