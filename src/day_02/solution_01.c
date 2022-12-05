872f69d672300f91cc839b4804f2505d5a8c3749
McCormick Wooden <mwooden3@gatech.edu>
2022-12-04 18:05:19 -0600

    cleanup

--- src/day_02/solution_01.c
+++ src/day_02/solution_01.c
@@ -1,128 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <assert.h>
 
 const char *INPUT_FILE_PATH = "input.txt";
 
-typedef struct moves {
-    int len;
-    char *oppMoves;
-    char *yourMoves;
-} moves_t;
-
-void countLine_itercbk(char *line, int lineNumber, void *returnptr) {
-    (*(int*)returnptr)++;
-}
-
-void populateMoves_itercbk(char *line, int lineNumber, void *returnptr) {
-    moves_t *moves = (moves_t*)returnptr;
-
-    moves->oppMoves[lineNumber] = line[0];
-
-    // normalize
-    char normalized = '\0';
-    if (line[2] == 'X')
-        normalized = 'A';
-    else if (line[2] == 'Y')
-        normalized = 'B';
-    else if (line[2] == 'Z')
-        normalized = 'C';
-    moves->yourMoves[lineNumber] = normalized;
-}
-
-void fileIterator(FILE *fp, void (*callback)(), void *cbkretptr) {
-    char* line = NULL;
-    size_t len = 0;
-    ssize_t read;
-    int lineNumber = 0;
-
-    rewind(fp);
-    while((read = getline(&line, &len, fp)) != -1) {
-        (*callback)(line, lineNumber, cbkretptr);
-        lineNumber++;
-    }
-    free(line); // gets malloc'd in getline so we have to free
-    rewind(fp);
-}
-
-int calculateScore(moves_t *theMoves) {
-    int totalScore = 0;
-    const int YOUR_MOVE_SCORE_ASCII_OFFSET = 64;
-    const int WIN_SCORE = 6;
-    const int DRAW_SCORE = 3;
-    const int LOSS_SCORE = 0;
-
-    for (int i = 0; i < theMoves->len; i++) {
-
-        char oppMove = theMoves->oppMoves[i];
-        char yourMove = theMoves->yourMoves[i];
-        int yourMoveScore = yourMove - YOUR_MOVE_SCORE_ASCII_OFFSET; // 'A', 'B', 'C' ascii codes = 65, 66, 67
-
-        int winLossScore = DRAW_SCORE;
-        switch(oppMove) {
-            case 'A':
-                switch(yourMove) {
-                    case 'B':
-                        winLossScore = WIN_SCORE;
-                        break;
-                    case 'C':
-                        winLossScore = LOSS_SCORE;
-                        break;
-                }
-                break;
-            case 'B':
-                switch(yourMove) {
-                    case 'A':
-                        winLossScore = LOSS_SCORE;
-                        break;
-                    case 'C':
-                        winLossScore = WIN_SCORE;
-                        break;
-                }
-                break;
-            case 'C':
-                switch(yourMove) {
-                    case 'A':
-                        winLossScore = WIN_SCORE;
-                        break;
-                    case 'B':
-                        winLossScore = LOSS_SCORE;
-                        break;
-                }
-                break;
-        }
-
-        totalScore = totalScore + winLossScore + yourMoveScore;
-    }
-
-    return totalScore;
-}
-
 int main(void) {
     FILE *fp = NULL;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    int solution = 0;
+
     if ((fp = fopen(INPUT_FILE_PATH, "r")) == NULL) {
         fprintf(stderr, "Can't find %s\n", INPUT_FILE_PATH);
         return EXIT_FAILURE;
     }
 
-    // get file length
-    int lineCount = 0;
-    fileIterator(fp, &countLine_itercbk, &lineCount);
+    const int YOUR_MOVE_SCORE_ASCII_OFFSET = -64;
+    const int WIN_SCORE = 6;
+    const int DRAW_SCORE = 3;
+    const int LOSS_SCORE = 0;
+    int winLossScore = DRAW_SCORE;
+    char whoBeatsWho[256] = {0};
+    char moveTranslations[256] = {0};
+    whoBeatsWho['A'] = 'C';
+    whoBeatsWho['B'] = 'A';
+    whoBeatsWho['C'] = 'B';
+    moveTranslations['X'] = 'A';
+    moveTranslations['Y'] = 'B';
+    moveTranslations['Z'] = 'C';
 
-    // init main data structure to hold moves
-    char oppMoves[lineCount];
-    char yourMoves[lineCount];
-    memset(oppMoves, 0, sizeof lineCount);
-    memset(yourMoves, 0, sizeof lineCount);
-    moves_t theMoves = {.len = lineCount, .oppMoves = oppMoves, .yourMoves = yourMoves};
+    while((read = getline(&line, &len, fp)) != -1) {
+        char oppMove, yourMove;
+        assert(sscanf(line, "%c %c", &oppMove, &yourMove) == 2);
 
-    // populate moves data structure
-    fileIterator(fp, &populateMoves_itercbk, &theMoves);
+        yourMove = moveTranslations[(int)yourMove];
 
-    // done
-    printf("solution: %d\n", calculateScore(&theMoves));
+        if (oppMove == yourMove) {
+            winLossScore = DRAW_SCORE;
+        } else {
+            if (whoBeatsWho[(int)yourMove] == oppMove) {
+                winLossScore = WIN_SCORE;
+            } else {
+                winLossScore = LOSS_SCORE;
+            }
+        }
+
+        solution += winLossScore + yourMove + YOUR_MOVE_SCORE_ASCII_OFFSET;
+    }
+
+    printf("solution: %d\n", solution);
 
     // cleanup
     fclose(fp);
+    free(line);
     return EXIT_SUCCESS;
 }
--- src/day_02/solution_02.c
+++ src/day_02/solution_02.c
@@ -1,159 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 const char *INPUT_FILE_PATH = "input.txt";
 
-typedef struct moves {
-    int len;
-    char *oppMoves;
-    char *yourMoves;
-} moves_t;
-
-void countLine_itercbk(char *line, int lineNumber, void *returnptr) {
-    (*(int*)returnptr)++;
-}
-
-char calculateYourMove(char oppMove, char outcome) {
-    char yourMove = '\0';
-
-    switch(outcome) {
-        case 'Y': // draw
-            yourMove = oppMove;
-            break;
-        case 'X': // lose
-            switch (oppMove)
-            {
-                case 'A':
-                    yourMove = 'C';
-                    break;
-                case 'B':
-                    yourMove = 'A';
-                    break;
-                case 'C':
-                    yourMove = 'B';
-                    break;
-            }
-            break;
-        case 'Z': // win
-            switch (oppMove)
-            {
-                case 'A':
-                    yourMove = 'B';
-                    break;
-                case 'B':
-                    yourMove = 'C';
-                    break;
-                case 'C':
-                    yourMove = 'A';
-                    break;
-            }
-            break;
-    }
-
-    return yourMove;
-}
-
-void populateMoves_itercbk(char *line, int lineNumber, void *returnptr) {
-    moves_t *moves = (moves_t*)returnptr;
-
-    moves->oppMoves[lineNumber] = line[0];
-    moves->yourMoves[lineNumber] = calculateYourMove(line[0], line[2]);
-}
-
-void fileIterator(FILE *fp, void (*callback)(), void *cbkretptr) {
-    char* line = NULL;
-    size_t len = 0;
-    ssize_t read;
-    int lineNumber = 0;
-
-    rewind(fp);
-    while((read = getline(&line, &len, fp)) != -1) {
-        (*callback)(line, lineNumber, cbkretptr);
-        lineNumber++;
-    }
-    free(line); // gets malloc'd in getline so we have to free
-    rewind(fp);
-}
-
-int calculateScore(moves_t *theMoves) {
-    int totalScore = 0;
-    const int YOUR_MOVE_SCORE_ASCII_OFFSET = 64;
-    const int WIN_SCORE = 6;
-    const int DRAW_SCORE = 3;
-    const int LOSS_SCORE = 0;
-
-    for (int i = 0; i < theMoves->len; i++) {
-
-        char oppMove = theMoves->oppMoves[i];
-        char yourMove = theMoves->yourMoves[i];
-        int yourMoveScore = yourMove - YOUR_MOVE_SCORE_ASCII_OFFSET; // 'A', 'B', 'C' ascii codes = 65, 66, 67
-
-        int winLossScore = DRAW_SCORE;
-        switch(oppMove) {
-            case 'A':
-                switch(yourMove) {
-                    case 'B':
-                        winLossScore = WIN_SCORE;
-                        break;
-                    case 'C':
-                        winLossScore = LOSS_SCORE;
-                        break;
-                }
-                break;
-            case 'B':
-                switch(yourMove) {
-                    case 'A':
-                        winLossScore = LOSS_SCORE;
-                        break;
-                    case 'C':
-                        winLossScore = WIN_SCORE;
-                        break;
-                }
-                break;
-            case 'C':
-                switch(yourMove) {
-                    case 'A':
-                        winLossScore = WIN_SCORE;
-                        break;
-                    case 'B':
-                        winLossScore = LOSS_SCORE;
-                        break;
-                }
-                break;
-        }
-
-        totalScore = totalScore + winLossScore + yourMoveScore;
-    }
-
-    return totalScore;
-}
-
 int main(void) {
     FILE *fp = NULL;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    int solution = 0;
+
     if ((fp = fopen(INPUT_FILE_PATH, "r")) == NULL) {
         fprintf(stderr, "Can't find %s\n", INPUT_FILE_PATH);
         return EXIT_FAILURE;
     }
 
-    // get file length
-    int lineCount = 0;
-    fileIterator(fp, &countLine_itercbk, &lineCount);
+    const int YOUR_MOVE_SCORE_ASCII_OFFSET = -64;
+    const int WIN_SCORE = 6;
+    const int DRAW_SCORE = 3;
+    const int LOSS_SCORE = 0;
+    int winLossScore = 0;
+    const char LOSS_CODE = 'X';
+    const char DRAW_CODE = 'Y';
+    const char WIN_CODE = 'Z';
+    char whoBeatsWho[256] = {0};
+    char whoLosesWho[256] = {0};
+    whoBeatsWho['A'] = 'C';
+    whoBeatsWho['B'] = 'A';
+    whoBeatsWho['C'] = 'B';
+    whoLosesWho['C'] = 'A';
+    whoLosesWho['A'] = 'B';
+    whoLosesWho['B'] = 'C';
 
-    // init main data structure to hold moves
-    char oppMoves[lineCount];
-    char yourMoves[lineCount];
-    memset(oppMoves, 0, sizeof lineCount);
-    memset(yourMoves, 0, sizeof lineCount);
-    moves_t theMoves = {.len = lineCount, .oppMoves = oppMoves, .yourMoves = yourMoves};
+    while((read = getline(&line, &len, fp)) != -1) {
+        char oppMove, moveCode, yourMove;
+        assert(sscanf(line, "%c %c", &oppMove, &moveCode) == 2);
 
-    // populate moves data structure
-    fileIterator(fp, &populateMoves_itercbk, &theMoves);
+        if (moveCode == DRAW_CODE) {
+            yourMove = oppMove;
+            winLossScore = DRAW_SCORE;
+        } else if (moveCode == LOSS_CODE) {
+            yourMove = whoBeatsWho[(int)oppMove];
+            winLossScore = LOSS_SCORE;
+        } else if (moveCode == WIN_CODE) {
+            yourMove = whoLosesWho[(int)oppMove];
+            winLossScore = WIN_SCORE;
+        } else {
+            assert(1);
+        }
 
-    // done
-    printf("solution: %d\n", calculateScore(&theMoves));
+        solution += winLossScore + yourMove + YOUR_MOVE_SCORE_ASCII_OFFSET;
+    }
+
+    printf("solution: %d\n", solution);
 
     // cleanup
     fclose(fp);
+    free(line);
     return EXIT_SUCCESS;
 }
