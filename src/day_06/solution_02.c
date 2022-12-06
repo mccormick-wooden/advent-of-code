a1565eb3d802019a6260a2fb71a59cd48e6333f1
mccormick.wooden <mccormick.wooden@betterment.com>
2022-12-05 23:07:14 -0600

    init day 6

---
+++ src/day_06/solution_02.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+static const char* INPUT_FILE_PATH = "input.txt";
+
+int main(void) {
+    FILE* fp = NULL;
+    char* line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    int solution = 0;
+
+    if ((fp = fopen(INPUT_FILE_PATH, "r")) == NULL) {
+        fprintf(stderr, "Can't find %s\n", INPUT_FILE_PATH);
+        return EXIT_FAILURE;
+    }
+
+    while ((read = getline(&line, &len, fp)) != -1) {
+
+    }
+
+    printf("solution: %d\n", solution);
+
+    // cleanup
+    fclose(fp);
+    free(line);
+    return EXIT_SUCCESS;
+}
