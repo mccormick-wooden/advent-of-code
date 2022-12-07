02c7dc2c987e01955a2b697f73fc18ed1027b05b
McCormick Wooden <mwooden3@gatech.edu>
2022-12-07 00:17:18 -0600

    init day 7

---
+++ src/day_07/solution_01.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+static const char *INPUT_FILE_PATH = "input.txt";
+
+int main(void) {
+    FILE *fp = NULL;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    int solution = 0;
+
+    if ((fp = fopen(INPUT_FILE_PATH, "r")) == NULL) {
+        fprintf(stderr, "Can't find %s\n", INPUT_FILE_PATH);
+        return EXIT_FAILURE;
+    }
+
+    while((read = getline(&line, &len, fp)) != -1) {
+        int lhlower, lhupper, rhlower, rhupper;
+
+        assert(sscanf(line, "%d-%d,%d-%d", &lhlower, &lhupper, &rhlower, &rhupper) == 4);
+
+        if ((lhupper <= rhupper && lhlower >= rhlower) || (rhupper <= lhupper && rhlower >= lhlower)) {
+            solution++;
+        }
+    }
+
+    printf("solution: %d\n", solution);
+
+    // cleanup
+    fclose(fp);
+    free(line);
+    return EXIT_SUCCESS;
+}
---
+++ src/day_07/solution_02.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+static const char* INPUT_FILE_PATH = "input.txt";
+
+int main(void) {
+    FILE* fp = NULL;
+    char* line = NULL;
+    size_t len = 0;
+    int solution = 0;
+    const int MAGIC_LEN = 14;
+    int offset = 0;
+
+    if ((fp = fopen(INPUT_FILE_PATH, "r")) == NULL) {
+        fprintf(stderr, "Can't find %s\n", INPUT_FILE_PATH);
+        return EXIT_FAILURE;
+    }
+
+    getline(&line, &len, fp);
+
+    while (1) {
+        char charsSeen[256] = {0};
+        int markerFound = 1;
+        for (int i = 0; i < MAGIC_LEN; i++) {
+            if (charsSeen[(int)line[i+offset]]) {
+                markerFound = 0;
+                break;
+            } else {
+                charsSeen[(int)line[i+offset]] = 1;
+            }
+        }
+
+        if (markerFound) {
+            break;
+        } else {
+            offset++;
+        }
+    }
+
+    solution = offset + MAGIC_LEN;
+    printf("solution: %d\n", solution);
+
+    // cleanup
+    fclose(fp);
+    free(line);
+    return EXIT_SUCCESS;
+}
